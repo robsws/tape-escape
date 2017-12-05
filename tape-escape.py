ec754afbd9d53eb74366a98eb6502eb042802467
Rob Streeting <robsws@gmail.com>
2017-12-05 23:47:02 +0000

    Loads a level map in from ini file.

--- tape-escape.py
+++ tape-escape.py
@@ -1,25 +1,45 @@
 from enum import Enum
 import pygame
-pygame.init()
+import configparser
 
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 600, 400
-GRID_WIDTH = 60
-GRID_HEIGHT = 40
-SCREEN_BORDER = 5
+GRID_WIDTH = 30
+GRID_HEIGHT = 20
+SCREEN_BORDER = 4
 
 BLACK      =   0,   0,   0
 DARK_GREY  =  30,  30,  30
 LIGHT_GREY = 100, 100, 100
 RED        = 150,   0,   0
 
-TILE_WIDTH = SCREEN_WIDTH/GRID_WIDTH
-TILE_BORDER = 5
+TILE_BORDER = 2
 
 class TileType(Enum):
     SPACE  = 1
     WALL   = 2
     PLAYER = 3
 
+config_tile_type_map = {
+    '.': TileType.SPACE,
+    'x': TileType.WALL,
+    'P': TileType.PLAYER
+}
+
+pygame.init()
+config = configparser.ConfigParser()
+config.read('levels.ini')
+
+# Build the internal level grid from config
+levelmap = config['Levels']['1']
+lines = levelmap.splitlines()
+grid_width = len(lines[0])
+grid_height = len(lines)
+grid = [[TileType.SPACE for y in range(grid_height)] for x in range(grid_width)]
+for y, line in enumerate(levelmap.splitlines()):
+    for x, tile in enumerate(line):
+        grid[x][y] = config_tile_type_map[tile]
+
+tile_width = SCREEN_WIDTH/grid_width
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
 finished = False
@@ -29,24 +49,15 @@ while not finished:
     
     screen.fill(BLACK)
 
-    # Create and draw a grid of 60 x 40 squares
-    # All grid squares are spaces to start
-    grid = [[TileType.SPACE for y in range(GRID_HEIGHT)] for x in range(GRID_WIDTH)]
-    for x in range(10):
-        grid[20+x][20] = TileType.WALL
-        grid[20][20+x] = TileType.WALL
-
-    grid[50][20] = TileType.PLAYER
-
-    for x in range(SCREEN_BORDER, GRID_WIDTH - SCREEN_BORDER):
-        for y in range(SCREEN_BORDER, GRID_HEIGHT - SCREEN_BORDER):
+    for x in range(grid_width):
+        for y in range(grid_height):
             tiletype = grid[x][y]
             if tiletype == TileType.SPACE or tiletype == TileType.PLAYER:
-                screen.fill(DARK_GREY, [int(x * TILE_WIDTH + TILE_BORDER/2), int(y * TILE_WIDTH + TILE_BORDER/2), int(TILE_WIDTH - TILE_BORDER/2), int(TILE_WIDTH - TILE_BORDER/2)], 0)
+                screen.fill(DARK_GREY, [x * tile_width + TILE_BORDER, y * tile_width + TILE_BORDER, tile_width - TILE_BORDER*2, tile_width - TILE_BORDER*2], 0)
             elif tiletype == TileType.WALL:
-                screen.fill(LIGHT_GREY, [x * TILE_WIDTH, y * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH], 0)
+                screen.fill(LIGHT_GREY, [x * tile_width, y * tile_width, tile_width, tile_width], 0)
             if tiletype == TileType.PLAYER:
-                pygame.draw.circle(screen, RED, (int(x * TILE_WIDTH + TILE_WIDTH/2), int(y * TILE_WIDTH + TILE_WIDTH/2)), int(TILE_WIDTH/2), 0)
+                pygame.draw.circle(screen, RED, (int(x * tile_width + tile_width/2), int(y * tile_width + tile_width/2)), int(tile_width/2), 0)
 
     pygame.display.flip()
 
