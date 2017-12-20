79367f78b6c6b29493e66ab6695ba00dd3fd6436
Rob Streeting <robsws@gmail.com>
2017-12-20 21:24:52 +0000

    Now draws the tape end. Also updated the loop to draw player based on player position variable rather than tile type.

--- tape-escape.py
+++ tape-escape.py
@@ -11,6 +11,7 @@ BLACK      =   0,   0,   0
 DARK_GREY  =  30,  30,  30
 LIGHT_GREY = 100, 100, 100
 RED        = 150,   0,   0
+YELLOW     = 150, 150,   0
 
 TILE_BORDER = 2
 
@@ -18,6 +19,10 @@ class TileType(Enum):
     SPACE  = 1
     WALL   = 2
     PLAYER = 3
+
+class PlayerOrientation(Enum):
+    LEFT  = 1
+    RIGHT = 2
 
 config_tile_type_map = {
     '.': TileType.SPACE,
@@ -29,6 +34,12 @@ pygame.init()
 config = configparser.ConfigParser()
 config.read('levels.ini')
 
+# Variables to represent player state
+player_position = (0,0)
+player_direction = (0,-1)
+player_orientation = -1 # -1 for left, 1 for right
+tape_end_position = (0,0)
+
 # Build the internal level grid from config
 levelmap = config['Levels']['1']
 lines = levelmap.splitlines()
@@ -38,9 +49,23 @@ grid = [[TileType.SPACE for y in range(grid_height)] for x in range(grid_width)]
 for y, line in enumerate(levelmap.splitlines()):
     for x, tile in enumerate(line):
         grid[x][y] = config_tile_type_map[tile]
+        if config_tile_type_map[tile] == TileType.PLAYER:
+            player_position = (x,y)
+            tape_end_position = (x,y)
 
 tile_width = SCREEN_WIDTH/grid_width
 screen = pygame.display.set_mode(SCREEN_SIZE)
+
+# Subroutines for updating state based on input
+# def extend_tape():
+
+# def retract_tape():
+
+# def rotate_left():
+
+# def rotate_right():
+
+# def switch_orientation():
 
 finished = False
 while not finished:
@@ -55,9 +80,14 @@ while not finished:
             if tiletype == TileType.SPACE or tiletype == TileType.PLAYER:
                 screen.fill(DARK_GREY, [x * tile_width + TILE_BORDER, y * tile_width + TILE_BORDER, tile_width - TILE_BORDER*2, tile_width - TILE_BORDER*2], 0)
             elif tiletype == TileType.WALL:
-                screen.fill(LIGHT_GREY, [x * tile_width, y * tile_width, tile_width, tile_width], 0)
-            if tiletype == TileType.PLAYER:
+                screen.fill(LIGHT_GREY, [x * tile_width + TILE_BORDER, y * tile_width + TILE_BORDER, tile_width - TILE_BORDER*2, tile_width - TILE_BORDER*2], 0)
+            if player_position == (x,y):
                 pygame.draw.circle(screen, RED, (int(x * tile_width + tile_width/2), int(y * tile_width + tile_width/2)), int(tile_width/2), 0)
+            if tape_end_position == (x,y):
+                tape_end_centre = (int(x * tile_width + tile_width/2) + (player_direction[0] * tile_width/2), int(y * tile_width + tile_width/2) + (player_direction[1] * tile_width/2))
+                tape_end_point = tuple(int(sum(x)) for x in zip(tape_end_centre, (player_orientation * player_direction[1] * tile_width * 0.66, player_orientation * player_direction[0] * tile_width * 0.66)))
+                pygame.draw.line(screen, YELLOW, tape_end_centre, tape_end_point, 2)
+
 
     pygame.display.flip()
 
