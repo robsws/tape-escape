9e0ae12dbaf3369fe54bdf2f628588e89ac2d563
Rob Streeting <robsws@gmail.com>
2018-01-02 23:01:07 +0000

    Implemented basic extend_tape procedure - now tape end position will keep moving in player direction until a wall is detected in the way of the tape end.

--- tape-escape.py
+++ tape-escape.py
@@ -19,7 +19,15 @@ TAPE_LENGTH = 6
 
 def vector_add(vec1, vec2):
     # Add two 2d tuples elementwise
-    return (sum(x) for x in zip(vec1, vec2))
+    return tuple(sum(x) for x in zip(vec1, vec2))
+
+def vector_scalar_multiply(vec, scalar):
+    # Multiply 2d tuple by scalar elementwise
+    return tuple(x * scalar for x in vec)
+
+def rotate_right(vec):
+    # Give the compass vector rotated 90 degrees clockwise
+    return (vec[1] * -1, vec[0])
 
 class TileType(Enum):
     SPACE  = 1
@@ -40,7 +48,7 @@ class GameState:
         }
         self.player_position = (0,0)
         self.player_direction = (0,-1)
-        self.player_orientation = -1 # -1 for left, 1 for right
+        self.player_orientation = 1 # -1 for left, 1 for right
         self.tape_end_position = (0,0)
 
         # Build the internal level grid from config
@@ -56,10 +64,31 @@ class GameState:
                     self.tape_end_position = (x,y)
 
     # Methods for updating state based on input
-    # def extend_tape(self):
-    #     # tape goes as far forward as possible, then pushes player back
-    #     temp_tape_end_pos = tape_end_position
-    #     while grid[tape_en]
+    def extend_tape(self):
+        # tape goes as far forward as possible, TODO(then pushes player back)
+        tape_end_position = self.tape_end_position
+        prev_tape_end_position = tape_end_position
+        # tape edge is where the very tip of the tape end resides, the adjacent square to the tape end position.
+        #
+        #           0 1 2    
+        #         0   r -    
+        #         1   |      
+        #         2   O
+        #
+        # tape end position (r) = (1,0)
+        # tape edge position (-) = (2,0)
+        # player position (O) = (1,2)
+        tape_edge_offset = vector_scalar_multiply(rotate_right(self.player_direction), self.player_orientation)
+        tape_edge_position = vector_add(tape_end_position, tape_edge_offset)
+        while self.grid[tape_end_position[0]][tape_end_position[1]] != TileType.WALL and self.grid[tape_edge_position[0]][tape_edge_position[1]] != TileType.WALL:
+            prev_tape_end_position = tape_end_position
+            tape_end_position = vector_add(tape_end_position, self.player_direction)
+            tape_edge_position = vector_add(tape_end_position, tape_edge_offset)
+            print(tape_end_position, tape_edge_position)
+        # we want the tape to end up behind the wall, so use prev tape end position
+        tape_end_position = prev_tape_end_position
+        print(tape_end_position)
+        self.tape_end_position = tape_end_position
 
     # def retract_tape(self):
 
@@ -74,20 +103,24 @@ config = configparser.ConfigParser()
 config.read('levels.ini')
 level = config['Levels']['1']
 state = GameState(level)
+state.extend_tape()
 
 tile_width = SCREEN_WIDTH/state.grid_width
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
+# Main game loop
 finished = False
 while not finished:
+    # Quit game if QUIT signal is detected
     for event in pygame.event.get():
         if event.type == pygame.QUIT: finished = True
     
+    # Reset screen to black
     screen.fill(BLACK)
 
+    # Draw the grid and the objects to the pygame screen
     for x in range(state.grid_width):
         for y in range(state.grid_height):
-            # Draw the grid and the objects to the pygame screen
             tiletype = state.grid[x][y]
             if tiletype == TileType.SPACE or tiletype == TileType.PLAYER:
                 screen.fill(DARK_GREY, [x * tile_width + TILE_BORDER, y * tile_width + TILE_BORDER, tile_width - TILE_BORDER*2, tile_width - TILE_BORDER*2], 0)
@@ -95,11 +128,13 @@ while not finished:
                 screen.fill(LIGHT_GREY, [x * tile_width + TILE_BORDER, y * tile_width + TILE_BORDER, tile_width - TILE_BORDER*2, tile_width - TILE_BORDER*2], 0)
             if state.tape_end_position == (x,y):
                 tape_end_centre = (int(x * tile_width + tile_width/2) + (state.player_direction[0] * tile_width/2), int(y * tile_width + tile_width/2) + (state.player_direction[1] * tile_width/2))
-                tape_end_point = tuple(int(sum(x)) for x in zip(tape_end_centre, (state.player_orientation * state.player_direction[1] * tile_width * 0.66, state.player_orientation * state.player_direction[0] * tile_width * 0.66)))
-                pygame.draw.line(screen, YELLOW, tape_end_centre, tape_end_point, 2)
+                tape_edge_offset = vector_scalar_multiply(rotate_right(state.player_direction), state.player_orientation * tile_width * 0.66) 
+                tape_edge = vector_add(tape_end_centre, tape_edge_offset)
+                pygame.draw.line(screen, YELLOW, tape_end_centre, tape_edge, 2)
             if state.player_position == (x,y):
                 pygame.draw.circle(screen, RED, (int(x * tile_width + tile_width/2), int(y * tile_width + tile_width/2)), int(tile_width/2), 0)
 
+    # Swap the buffers
     pygame.display.flip()
 
 pygame.quit()
